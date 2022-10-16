20664a66947a95fe4523ee61cdfdf5955a961908
zacharba <zacharba@mail.uc.edu>
2022-10-16 12:18:54 -0400

    Added a few templates

--- popup.js
+++ popup.js
@@ -3,7 +3,29 @@ chrome.tabs.query({
     lastFocusedWindow: true
 }, function(tabs) {
     var tab = tabs[0];
-    console.log(tab.url);
     document.getElementById("curl").innerHTML = "Current Tab: " + (tab.url);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("accountPage").addEventListener("click", handler);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("newPopUp").addEventListener("click", newpop);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("back").addEventListener("click", return2Sender);
+});
+ 
+function handler() {
+    window.open("cryptopass-Accounts.html", '_blank');
+}
+
+function newpop() {
+    window.location.href="popupTest.html";
+}
+
+function return2Sender(){
+    window.location.href="popup.html";
+}
