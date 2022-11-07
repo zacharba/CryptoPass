ac85f6e787ee7d4576ae241d39991b37213bbc51
zacharba <zacharba@mail.uc.edu>
2022-11-07 18:44:31 -0500

    Added Firebase Template

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFireStore, collection, getDocs } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = {
   apiKey: "AIzaSyAHYh_IWXPp_mcbA-g71NXCIKN6pz9L0U4",
   authDomain: "cryptopass-81d7d.firebaseapp.com",
@@ -16,13 +10,23 @@ const firebaseApp = {
   storageBucket: "cryptopass-81d7d.appspot.com",
   messagingSenderId: "22180737663",
   appId: "1:22180737663:web:109d78966f1ec23d3e854a",
-  measurementId: "G-8W7F63WX8R"
+  measurementId: "G-8W7F63WX8R",
+  databaseURL: "https://cryptopass-81d7d-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseApp);
-const analytics = getAnalytics(app);
-const auth = getAuth(firebaseApp);
-const db = getFireStore(firebaseApp);
-const todosCol = collection(db);
-const snapshot = await getDocs(todosCol);
+const database = getDatabase(app);
+
+
+function writeAccount(userId, website, uname, stat, pass) {
+  const db = getDatabase();
+  set(ref(db, 'users/' + userId), {
+    url: website,
+    username: uname,
+    password: pass,
+    status: stat
+  });
+}
+
+writeAccount("Brandon", "www.example.com", "test@example.com", "test123")
